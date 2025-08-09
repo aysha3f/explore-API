9d7be4b984b43011f5d5ada7f6f1b9d5872caabd
aysha3f <tahiaysha220@gmail.com>
2025-08-09 08:43:21 +0600

    error

--- js/comment.js
+++ js/comment.js
@@ -7,7 +7,12 @@ fetch('https://jsonplaceholder.typicode.com/comments')
 
 
 const loadComments2 = async () => {
-    const res = await fetch('https://jsonplaceholder.typicode.com/comments');
+  try{
+      const res = await fetch('https://jsonplaceholder.typicode.com/comments');
     const data = await res.json();
     console.log(data);
+  }
+  catch(error){
+    console.error('data load error');
+  }
 }
